c5a153a5eb2d1d0ebe727193110f6b6209ad0026
myh2910 <minleeyohan@gmail.com>
2021-09-24 14:37:35 -0500

    spacing

--- config.py
+++ config.py
@@ -9,37 +9,36 @@ def open_editor(filename):
 
 def latex_document(latex):
 	return r"""
-\documentclass[10pt,border=10pt]{standalone}
-\usepackage[nosetup,nothm]{evan}
+	\documentclass[10pt,border=10pt]{standalone}
+	\usepackage[nosetup,nothm]{evan}
 
-\usepackage[utf8]{inputenc}
-\usepackage[T1]{fontenc}
-\usepackage{mhchem}
+	\usepackage[utf8]{inputenc}
+	\usepackage[T1]{fontenc}
+	\usepackage{mhchem}
 
-\begin{document}
-""" + latex + r"\end{document}"
+	\begin{document}
+	""" + latex + r"\end{document}"
 
 config = {
-    # For example '~/.config/rofi/ribbon.rasi' or None
-    'rofi_theme': None,
-    # Font that's used to add text in inkscape
-    'font': 'monospace',
-    'font_size': 10,
-    'open_editor': open_editor,
-    'latex_document': latex_document,
+	# For example '~/.config/rofi/ribbon.rasi' or None
+	'rofi_theme': None,
+	# Font that's used to add text in inkscape
+	'font': 'monospace',
+	'font_size': 10,
+	'open_editor': open_editor,
+	'latex_document': latex_document,
 }
-
 
 # From https://stackoverflow.com/a/67692
 def import_file(name, path):
-    import importlib.util as util
-    spec = util.spec_from_file_location(name, path)
-    module = util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+	import importlib.util as util
+	spec = util.spec_from_file_location(name, path)
+	module = util.module_from_spec(spec)
+	spec.loader.exec_module(module)
+	return module
 
 CONFIG_PATH = Path('~/.config/inkscape-shortcut-manager').expanduser()
 
 if (CONFIG_PATH / 'config.py').exists():
-    userconfig = import_file('config', CONFIG_PATH / 'config.py').config
-    config.update(userconfig)
+	userconfig = import_file('config', CONFIG_PATH / 'config.py').config
+	config.update(userconfig)
--- text.py
+++ text.py
@@ -3,20 +3,20 @@ import normal
 from time import sleep
 
 def text_mode(self, event, char):
-    """
-    'text mode' for when you just want to type text, i.e. 'disabled mode'.
+	"""
+	'text mode' for when you just want to type text, i.e. 'disabled mode'.
 
-    """
-    if char and char == '`':
-        """
-        Go to normal mode again!
-        """
-        self.press('Escape')
-        sleep(0.1)
-        self.press('Escape')
-        self.mode = normal.normal_mode
-        return 
+	"""
+	if char and char == '`':
+		"""
+		Go to normal mode again!
+		"""
+		self.press('Escape')
+		sleep(0.1)
+		self.press('Escape')
+		self.mode = normal.normal_mode
+		return 
 
-    self.inkscape.send_event(event, propagate = True)
-    self.disp.flush()
-    self.disp.sync()
+	self.inkscape.send_event(event, propagate = True)
+	self.disp.flush()
+	self.disp.sync()
